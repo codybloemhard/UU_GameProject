5fb2d94d18504f2c5b35fbf6fdcf83993187a659
Cody Bloemhard <cblmhrd@live.nl>
2018-01-01 15:00:47 +0100

    update commit
    ok

--- UU_GameProject/Generation/ShowCaseScene.cs
+++ UU_GameProject/Generation/ShowCaseScene.cs
@@ -15,7 +15,7 @@ namespace UU_GameProject
         {
             GameStateManager.SetRenderingMode(BlendState.NonPremultiplied, SamplerState.PointWrap);
             Debug.ProfilingMode();
-            GameObject backg = new GameObject(this, 10, true);
+            GameObject backg = new GameObject(this, 20, true);
             backg.AddComponent(new CRender("block"));
             backg.Renderer.colour = new Color(0.7f, 0.7f, 0.7f);
             backg.Pos = Vector2.Zero;
@@ -46,22 +46,22 @@ namespace UU_GameProject
                 if (old != null) foreach (GameObject o in old) o.Destroy();
                 old = objects.FindAllWithTag("_child");
                 if (old != null) foreach (GameObject o in old) o.Destroy();
-                Catalog.CreateBlock(this, 0x0, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE);
-                Catalog.CreateBlock(this, 0x1, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE, TOPTILES.GRASS);
-                Catalog.CreateBlock(this, 0x2, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.ICE, LAYERTILES.NONE);
-                Catalog.CreateBlock(this, 0x3, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.ICETOP, LAYERTILES.NONE);
-                Catalog.CreateBlock(this, 0x4, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.ICETOP, LAYERTILES.NONE, TOPTILES.SNOW);
-                Catalog.CreateBlock(this, 0x5, 0, 0, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE, TOPTILES.SNOW);
-                Catalog.CreateBlock(this, 0x6, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE);
-                Catalog.CreateBlock(this, 0x7, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICE);
-                Catalog.CreateBlock(this, 0x8, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICETOP);
-                Catalog.CreateBlock(this, 0x9, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICETOP, TOPTILES.SNOW);
-                Catalog.CreateBlock(this, 0xA, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICE, TOPTILES.SNOW);
-                Catalog.CreateBlock(this, 0xB, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE, TOPTILES.SNOW);
-                Catalog.CreateBlock(this, 0xC, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.CRACKS);
-                Catalog.CreateBlock(this, 0xD, 0, 0, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE, TOPTILES.GRASS);
-                Catalog.CreateBlock(this, 0xE, 0, 0, "_test", BASETILES.SAND);
-                Catalog.CreateBlock(this, 0xF, 0, 0, "_test", BASETILES.SANDSTONE, LAYERTILES.CRACKS);
+                Catalog.CreateBlock(this, 0x0, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE);
+                Catalog.CreateBlock(this, 0x1, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE, TOPTILES.GRASS);
+                Catalog.CreateBlock(this, 0x2, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.ICE, LAYERTILES.NONE);
+                Catalog.CreateBlock(this, 0x3, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.ICETOP, LAYERTILES.NONE);
+                Catalog.CreateBlock(this, 0x4, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.ICETOP, LAYERTILES.NONE, TOPTILES.SNOW);
+                Catalog.CreateBlock(this, 0x5, 0, 10, "_test", BASETILES.DIRT, LAYERTILES.NONE, LAYERTILES.NONE, TOPTILES.SNOW);
+                Catalog.CreateBlock(this, 0x6, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE);
+                Catalog.CreateBlock(this, 0x7, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICE);
+                Catalog.CreateBlock(this, 0x8, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICETOP);
+                Catalog.CreateBlock(this, 0x9, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICETOP, TOPTILES.SNOW);
+                Catalog.CreateBlock(this, 0xA, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.ICE, TOPTILES.SNOW);
+                Catalog.CreateBlock(this, 0xB, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE, TOPTILES.SNOW);
+                Catalog.CreateBlock(this, 0xC, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.CRACKS);
+                Catalog.CreateBlock(this, 0xD, 0, 10, "_test", BASETILES.STONE, LAYERTILES.CRACKS, LAYERTILES.NONE, TOPTILES.GRASS);
+                Catalog.CreateBlock(this, 0xE, 0, 10, "_test", BASETILES.SAND);
+                Catalog.CreateBlock(this, 0xF, 0, 10, "_test", BASETILES.SANDSTONE, LAYERTILES.CRACKS);
 
                 Catalog.CreateTree0(this, new Vector2(2, 9), new Vector2(0.1f), 0, "tree");
                 Catalog.CreateTree1(this, new Vector2(5, 9), new Vector2(0.2f), 0, "tree");
